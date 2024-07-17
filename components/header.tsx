49a9afa45f830e92142e8a8239029aea3bf509fd
Pratul Makar <kk953504@gmail.com>
2024-07-17 20:22:41 +0530

    1

--- components/header.tsx
+++ components/header.tsx
@@ -7,8 +7,8 @@ import MobileNav from '@/components/mobileNav'
 
 const Header = () => {
   return (
-    <header className='fixed z-50 w-full bg-primary py-6 xl:py-8'>
-      <div className='container mx-auto flex items-center justify-between'>
+    <header className='fixed z-50 w-full bg-primary py-3 xl:py-4'>
+      <div className='container mx-auto flex cursor-pointer items-center justify-between'>
         {/* logo */}
         <Link to='/' spy={true} smooth={true} className='cursor-pointer'>
           <Image src='/logo.svg' height={54.53} width={41.35} alt='logo' />
@@ -18,7 +18,7 @@ const Header = () => {
         <div className='hidden items-center gap-x-8 xl:flex'>
           <Nav />
           <Link
-            to='/'
+            to='Contact'
             spy={true}
             smooth={true}
             className='text-md rounded-lg border-2 px-5 py-2 font-medium text-[#db511b] transition-all hover:border-[#db511b] hover:bg-[#db511b] hover:text-white'
